352ce87d2563b31ee99b85863d529cefcd6cfbf4
SamiraHofer <samirahofer@hispeed.ch>
2026-02-26 11:12:31 +0100

    Autor angepasst

--- views/home.py
+++ views/home.py
@@ -11,5 +11,5 @@ Diese App wurde von folgenden Personen entwickelt:
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
-Autor: Samuel Wehrli (wehs@zhaw.ch)
+Autor: Samira Hofer (hofersa5@students.zhaw.ch)
 """
